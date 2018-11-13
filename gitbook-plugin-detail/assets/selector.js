b287a282c21bdc90fd13a3398aa09bf32658dbb5
Joona Yoon <joonas.yoon@gmail.com>
2018-11-13 13:52:51 +0900

    bugfix on max-height

--- gitbook-plugin-detail/assets/selector.js
+++ gitbook-plugin-detail/assets/selector.js
@@ -1,18 +1,21 @@
 function addSelector() {
   var detail = document.querySelectorAll('.detail');
-  Object.keys(detail)
-    .forEach(function(index) {
-      detail[index].onclick = function(event) {
-        if(event.target.className !== 'dtitle') {
-          return null;
-        }
+  detail.forEach(function(el){
+    var btn = el.querySelector('.dtitle');
+    btn.onclick = function(event){
+      event.stopPropagation();
 
-        event.stopPropagation();
+      var parent = event.target.parentNode;
+      parent.className = ~parent.className.indexOf('dclose') ? 'detail' : 'detail dclose';
 
-        detail[index].className = ~detail[index].className.indexOf('dclose') ? 'detail' : 'detail dclose';
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + 'px';
       }
-    
-    });
+    };
+  });
 }
 
 require(['gitbook'], function (gitbook) {
