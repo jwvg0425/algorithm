77948fbb5697a92da25fd8f568e5eba7a442d28a
Joona Yoon <joonas.yoon@gmail.com>
2018-11-13 14:56:16 +0900

    bugfix - resize height automatically

--- gitbook-plugin-detail/assets/selector.js
+++ gitbook-plugin-detail/assets/selector.js
@@ -1,5 +1,13 @@
 function addSelector() {
   var detail = document.querySelectorAll('.detail');
+
+  function resize() {
+    detail.forEach(function(el){
+      var content = el.querySelector('.dcontent');
+      content.style.maxHeight = content.scrollHeight + 'px';
+    });
+  };
+
   detail.forEach(function(el){
     var btn = el.querySelector('.dtitle');
     btn.onclick = function(event){
@@ -9,11 +17,10 @@ function addSelector() {
       parent.className = ~parent.className.indexOf('dclose') ? 'detail' : 'detail dclose';
 
       var content = this.nextElementSibling;
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + 'px';
-      }
+      content.style.maxHeight = content.scrollHeight + 'px';
+
+      // after css animation
+      setTimeout(resize, 500);
     };
   });
 }
